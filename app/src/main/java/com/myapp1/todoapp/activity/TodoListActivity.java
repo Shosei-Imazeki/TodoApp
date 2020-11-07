ffebbb90daaede750d2553813863156449742cb4
Shosei-Imazeki <mon.sho.de@gmail.com>
2020-11-07 15:11:34 +0900

    [setup]3画面の基本的なファイルを作成

---
+++ app/src/main/java/com/myapp1/todoapp/activity/TodoListActivity.java
@@ -0,0 +1,16 @@
+package com.myapp1.todoapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.myapp1.todoapp.R;
+
+public class TodoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
